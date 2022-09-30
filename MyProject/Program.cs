eaa6558222c169f6a979fc9f348e89e2ab271ada
Your Name <polliandfibi@gmail.com>
2022-09-30 13:09:16 +0300

    first commit

---
+++ MyProject/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void CreateArray(string[,] arr, int n, int m)
+        {
+            arr = new string[n, m]; // Пустой двухмерный массив Команд с Игроками
+
+            Console.WriteLine("Команды и игроки по порядку: \n");
+            for (int i = 0; i < n; i++) // Заполнение массива Командами и Игроками
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    arr[i, j] = " |К" + (i + 1).ToString() + "И" + (j + 1).ToString() + "|"; // Игроки имеют тип Стринг и содержат номер своей команды и свой номер
+                    Console.Write(arr[i, j]); // Вывод на экран
+                }
+                Console.WriteLine();
+
+                for (int l = 0; l < m; l++) // Для сохрания разметки поля
+                {
+                    Console.Write("-------");
+                }
+                Console.WriteLine();
+
+            }
+            //Console.WriteLine();
+        }
+
+        static void ReverseArray(string[,] newArr, string[,] arr, int n, int m)
+        {
+
+            newArr = new string[m, n]; // Создание нового массива с новой расстановкой игроков
+
+            arr = new string[n, m]; // Пустой двухмерный массив Команд с Игроками
+
+
+            for (int i = 0; i < n; i++) // Заполнение массива Командами и Игроками
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    arr[i, j] = " |К" + (i + 1).ToString() + "И" + (j + 1).ToString() + "|"; // Игроки имеют тип Стринг и содержат номер своей команды и свой номер
+                                                                                             //  Console.Write(arr[i, j]); // Вывод на экран
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("Все игроки разные в рядках: \n");
+            for (int i = 0; i < newArr.GetLength(0); i++) // Разворот массива на 90 градусов. Этим достигается размещение игроков разных команд в ряды
+            {
+                for (int j = 0; j < newArr.GetLength(1); j++)
+                {
+                    newArr[i, j] = arr[j, i];
+                    Console.Write(newArr[i, j]);
+                }
+                Console.WriteLine();
+
+
+                for (int l = 0; l < n; l++) // Вывод на экран с учетом первого требования задачи
+                {
+                    Console.Write("-------");
+                }
+                Console.WriteLine();
+            }
+           
+        }
+
+       
+
+
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine()); // Количество Команд
+            int m = int.Parse(Console.ReadLine());  // Количество Игроков
+
+            string[,] arr = new string[n, m]; // Пустой двухмерный массив Команд с Игроками
+            CreateArray(arr, n, m);
+            string[,] newArr = new string[m, n];
+
+
+            ReverseArray(newArr, arr, n, m);
+
+            Console.WriteLine("Расстановка спортсменов: \n");
+
+
+
+            for (int i = 0; i < newArr.GetLength(0); i++) // Сдвиг элементов массива для того чтобы игроки с одной команды не стояли друг под другом.
+            {
+
+                if (i % 2 == 0 && i % 4 != 0)
+                {
+                    string tmp = newArr[i, 0];
+
+                    for (int j = 1; j < newArr.GetLength(1); j++)
+                    {
+                        Console.Write(newArr[i, j]);
+                    }
+                    Console.Write(tmp);
+                }
+
+                if (i == 1)
+                {
+                    for (int j = 0; j < newArr.GetLength(1); j++)
+                    {
+                        Console.Write(newArr[i, j]);
+                    }
+                }
+
+
+                else if (i % 3 == 0)
+                {
+                    string tmp = newArr[i, 0];
+                    string tmp1 = newArr[i, 1];
+
+
+                    for (int j = 2; j < newArr.GetLength(1); j++)
+                    {
+                        Console.Write(newArr[i, j]);
+                    }
+                    Console.Write(tmp);
+                    Console.Write(tmp1);
+                }
+
+                else if (i % 4 == 0 && i > 2)
+                {
+                    string tmp = newArr[i, 0];
+                    string tmp1 = newArr[i, 1];
+                    string tmp2 = newArr[i, 2];
+
+                    for (int j = 3; j < newArr.GetLength(1); j++)
+                    {
+                        Console.Write(newArr[i, j]);
+                    }
+                    Console.Write(tmp);
+                    Console.Write(tmp1);
+                    Console.Write(tmp2);
+                }
+                else if (i % 5 == 0)
+                {
+                    string tmp = newArr[i, newArr.GetLength(1) - 1];
+
+                    for (int j = 0; j < newArr.GetLength(1) - 1; j++)
+                    {
+                        Console.Write(newArr[i, j]);
+                    }
+                    Console.Write(tmp);
+
+                }
+
+                Console.WriteLine();
+
+                for (int l = 0; l < n; l++) // Вывод на экран с учетом второго требования задачи
+                {
+                    Console.Write("-------");
+                }
+
+                Console.WriteLine();
+
+            }
+
+            Console.WriteLine("-------------------");
+            Console.WriteLine();
+
+
+        }
+    }
+}
